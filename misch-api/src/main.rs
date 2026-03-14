9465a4a8585a0796d1adf74a020eb59c80d193c6
Henry McBraida <hmcbraida@gmail.com>
2026-03-14 16:10:10 +0000

    Aborting on API panic

--- misch-api/src/main.rs
+++ misch-api/src/main.rs
@@ -1,7 +1,16 @@
 use rocket::launch;
 
+fn install_abort_on_panic_hook() {
+    let default_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        default_hook(panic_info);
+        std::process::abort();
+    }));
+}
+
 #[launch]
 /// Rocket launch hook for the API binary.
 fn rocket() -> _ {
+    install_abort_on_panic_hook();
     misch_api::build_rocket()
 }
